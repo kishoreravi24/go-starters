62869d3cb66639c10e41020e2f7d0adffe591e67
kishoreravi24 <kishoreravi24399@gmail.com>
2023-12-29 09:56:49 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName, lastName string = "John", "due"
+	var age int = 20
+	var (
+		name     string = "dave"
+		user_age int    = 21
+	)
+	fmt.Println("Hello World!")
+	fmt.Println(firstName, lastName, age)
+	fmt.Println(name, user_age)
+}
