eb2b79206d0e39dd7caff1e2b238eb9351904ec5
kishoreravi24 <kishoreravi24399@gmail.com>
2024-01-02 05:41:44 +0530

    Pointers & type

--- variables/variables.go
+++ variables/variables.go
@@ -3,6 +3,7 @@ package variables
 import "fmt"
 
 func Variables() {
+	// var name type = expression
 	var firstName, lastName string = "John", "due"
 	var age int = 20
 	var (
@@ -11,4 +12,32 @@ func Variables() {
 	)
 	fmt.Println(firstName, lastName, age)
 	fmt.Println(name, user_age)
+
+	// name := expression
+	content_in := "go"
+	fmt.Println(content_in)
+
+	// Pointers in short variable decleration
+	x := 1
+	p := &x
+	fmt.Println(p)
+	fmt.Println(*p)
+
+	// Pointers
+	var y int = 2
+	var point *int = &y
+	fmt.Println(point)
+	fmt.Println(*point)
+
+	// Type decleration
+	type fahrenheit int
+	type celsius int
+
+	var f fahrenheit = 32
+	var c celsius = 0
+
+	fmt.Println(f, c)
+
+	c = celsius((f - 32) * 5 / 9)
+	fmt.Println(f, c)
 }
